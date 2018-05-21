68c0e151406e2851e3ae11559027782eff2b1423
bookn <bookn@jitta.com>
2018-05-21 18:57:25 +0700

    logic for edit and add (will be refactored later :joy:)

--- adminScript.js
+++ adminScript.js
@@ -1,3 +1,5 @@
+const createElement = tag => document.createElement(tag)
+
 firebase.initializeApp(config.firebase)
 const messageRef = firebase.database().ref()
 
@@ -5,28 +7,77 @@ const displayQuestion = (questionIndex) => {
   firebase.database().ref('currentQuestion').set(questionIndex)
 }
 
-const editQuestion = (editIndex) => {
-  document.getElementById('modal').style.display = 'block'
+const saveQuestion = async (editIndex) => {
+  let author = document.getElementById('edit-author').value
+  let pictureUrl = document.getElementById('edit-pictureUrl').value
+  let text = document.getElementById('edit-text').value
+  const snapshot = await firebase.database().ref('questions').once('value')
+  const questions = snapshot.val()
+  questions[editIndex] = {
+    author,
+    pictureUrl,
+    text
+  }
+  firebase.database().ref('questions').set(questions)
+
+  author = ''
+  pictureUrl = ''
+  text = ''
+  document.getElementById('modal').style.display = 'none'
 }
 
-const createElement = (tag) => document.createElement(tag)
+const editQuestion = async (editIndex) => {
+  document.getElementById('modal').style.display = 'block'
+  const snapshot = await firebase.database().ref('questions').once('value')
+  const questions = snapshot.val()
+  const selectedQuestion = questions[editIndex]
+  console.log(selectedQuestion)
+
+  const authorInput = document.getElementById('edit-author')
+  const pictureUrlInput = document.getElementById('edit-pictureUrl')
+  const textInput = document.getElementById('edit-text')
+
+  authorInput.value = selectedQuestion.author
+  pictureUrlInput.value = selectedQuestion.pictureUrl
+  textInput.value = selectedQuestion.text
+
+  const editButton = document.getElementById('edit-button')
+  editButton.onclick = () => saveQuestion(editIndex)
+}
+
+const addQuestion = async () => {
+  let author = document.getElementById('add-author').value
+  let pictureUrl = document.getElementById('add-pictureUrl').value
+  let text = document.getElementById('add-text').value
+  const snapshot = await firebase.database().ref('questions').once('value')
+  const questions = snapshot.val()
+  questions.push({
+    author,
+    pictureUrl,
+    text
+  })
+  firebase.database().ref('questions').set(questions)
+  author = ''
+  pictureUrl = ''
+  text = ''
+}
 
 const renderQuestion = (question, pictureUrl, author, index) => {
-  const row = createElement("tr")
+  const row = createElement('tr')
 
-  const authorTd = createElement("td")
-  const questionTd = createElement("td")
-  const pictureTd = createElement("td")
-  const editTd = createElement("td")
-  const displayTd = createElement("td")
+  const authorTd = createElement('td')
+  const questionTd = createElement('td')
+  const pictureTd = createElement('td')
+  const editTd = createElement('td')
+  const displayTd = createElement('td')
 
-  const editButton = createElement("button")
-  const displayButton = createElement("button")
+  const editButton = createElement('button')
+  const displayButton = createElement('button')
   editButton.onclick = () => editQuestion(index)
   displayButton.onclick = () => displayQuestion(index)
   editButton.innerHTML = 'Edit'
   displayButton.innerHTML = 'Display this question'
-  
+
   authorTd.innerHTML = author
   questionTd.innerHTML = question
   pictureTd.innerHTML = `<img src="${pictureUrl}" style="width: 50px;">`
@@ -45,25 +96,19 @@ const renderQuestion = (question, pictureUrl, author, index) => {
 }
 
 messageRef.on('value', async (snapshot) => {
+  const addButton = document.getElementById('add-button')
+  addButton.onclick = addQuestion
+
   const { questions, currentQuestion } = snapshot.val()
 
   try {
     const tbody = document.getElementById('table-body')
     tbody.innerHTML = ''
-    questions.map((question, index) => {
-      const { text, pictureUrl, author} = question
+    questions.forEach((question, index) => {
+      const { text, pictureUrl, author } = question
       renderQuestion(text, pictureUrl, author, index)
     })
-    // const { videoId, commentId } = parseCommentUrl(commentUrl)
-    // const graphApiEndpoint = `https://graph.facebook.com/${videoId}_${commentId}?fields=message,id,created_time,from%7Bpicture.type(large),name%7D&access_token=${config.facebook.accessToken}`
-    // const response = await fetch(graphApiEndpoint)
-    // const json = await response.json()
-    // document.getElementById('result').innerHTML = questions[currentQuestion].questionText
-    // document.getElementById('profile-picture').src = questions[currentQuestion].profileUrl
-    // document.getElementById('author').innerHTML = questions[currentQuestion].author
   } catch (e) {
-    // document.getElementById('container').style.display = 'none'
-    // console.log('error occured', e)
-    // TODO: error page
+
   }
 })
