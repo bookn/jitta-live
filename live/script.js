b0ada6cbebb8538d80eeffcca741f2dab9bd5409
bookn <bookn@jitta.com>
2018-05-22 16:39:53 +0700

    add fade

--- live/script.js
+++ live/script.js
@@ -3,14 +3,19 @@ const messageRef = firebase.database().ref()
 
 messageRef.on('value', async (snapshot) => {
   const { questions, currentQuestion } = snapshot.val()
-  try {
-    document.getElementById('container').style.display = 'flex'
-    document.getElementById('result').innerHTML = questions[currentQuestion].text
-    document.getElementById('profile-picture').src = questions[currentQuestion].pictureUrl
-    document.getElementById('author').innerHTML = questions[currentQuestion].author
-  } catch (e) {
-    document.getElementById('container').style.display = 'none'
-    console.log('error occured', e)
-    // TODO: error page
-  }
+  const container = document.getElementById('container')
+  container.classList.add('fade')
+  setTimeout(() => {
+    try {
+      container.style.display = 'flex'
+      document.getElementById('result').innerHTML = questions[currentQuestion].text
+      document.getElementById('profile-picture').src = questions[currentQuestion].pictureUrl
+      document.getElementById('author').innerHTML = questions[currentQuestion].author
+      container.classList.remove('fade')
+    } catch (e) {
+      document.getElementById('container').style.display = 'none'
+      console.log('error occured', e)
+      // TODO: error page
+    }
+  }, 800)
 })
